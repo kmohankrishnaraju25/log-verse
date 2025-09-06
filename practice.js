e6eba49835c1a1d3610985d65ac8df3df489d061
kmohankrishnaraju25 <kmohankrishnaraju25@gamil.com>
2025-09-06 12:34:56 +0530

    hello

--- practice.js
+++ practice.js
@@ -1,1 +1,26 @@
 console.log("Hello World!");
+let a = "mohan"
+console.log(a)
+let num=18
+console.log(num)   
+console.log(typeof num)
+console.log(typeof a)
+let isLoggedIn = false;
+console.log(typeof isLoggedIn)
+let x
+console.log(x)
+console.log(typeof x)
+let y = null;
+console.log(y)
+console.log(typeof y)  
+let arr = [1,2,3,4]
+console.log(arr)
+console.log(typeof arr)
+let obj = {
+    name: "mohan",
+    age: 18,    
+}
+console.log(obj.name)
+console.log(obj.age)
+console.log(typeof obj)
+console.log(arr[0])
