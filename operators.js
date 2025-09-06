8b84c720a4dcd2488c12c501d580ea51a3327b67
kmohankrishnaraju25 <kmohankrishnaraju25@gamil.com>
2025-09-06 14:58:46 +0530

    operators

--- operators.js
+++ operators.js
@@ -1,6 +1,42 @@
+// arithmetic
 let a=10,b=15
 console.log(a+b)
 console.log(a-b)
 console.log(a*b)
 console.log(a/b)
 console.log(a%b)
+// comparison
+let c=10,d=30
+console.log(c==d)
+console.log(c>d)
+console.log(c<d)
+console.log(c>=d)
+console.log(c<=d)
+console.log(c!=d)
+console.log(c===d)
+console.log(c!==d)
+// logical
+console.log(a==c || b==d)
+console.log(a>30)
+// string concatenation
+let firstName = "mohan"
+let lastName = "krishna"
+console.log(firstName +" "+ lastName) 
+console.log(firstName)
+let correct = `HEllo ${firstName} ${lastName},welcome to js`
+console.log(correct)
+//ternary operator
+let age=18
+let drink = age>=18 ? "coffee" : "milk"
+console.log(drink)
+// increment and decrement
+let points_1=10
+points_1++
+console.log(points_1)
+let points_2=10
+++points_2
+console.log(points_2)
+// typeof operator
+console.log(typeof points_1)
+console.log(typeof firstName)
+console.log(typeof drink)
